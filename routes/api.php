f44b1c7b797b24919f1824942697c3d7757cd1fe
Zarathustra09 <joshua.pardo30@gmail.com>
2025-09-10 22:35:55 +0800

    Add route for user-specific reports and update DatabaseSeeder with password

--- routes/api.php
+++ routes/api.php
@@ -9,8 +9,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
+Route::get('/my-reports', [App\Http\Controllers\Api\CrimeReportController::class, 'myReports']);
 Route::middleware('auth:sanctum')->apiResource('crime-reports', App\Http\Controllers\Api\CrimeReportController::class);
 Route::middleware('auth:sanctum')->apiResource('announcements', App\Http\Controllers\Api\AnnouncementController::class);
+
+
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
@@ -27,5 +31,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/profile', [App\Http\Controllers\Api\ProfileController::class, 'destroy']);
 
 
-    Route::get('/crime-reports/my-reports', [App\Http\Controllers\Api\CrimeReportController::class, 'myReports']);
+
 });
+
